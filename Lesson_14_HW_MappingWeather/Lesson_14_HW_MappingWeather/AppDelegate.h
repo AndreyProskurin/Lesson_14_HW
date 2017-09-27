b8a59ceb0efd5900bd26f23972d6b2ffdb429cc8
Andrey Proskurin <andrewproskurin@gmail.com>
2017-09-27 21:32:27 +0300

    Empty single view app has been added

---
+++ Lesson_14_HW_MappingWeather/Lesson_14_HW_MappingWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_14_HW_MappingWeather
+//
+//  Created by Andrey Proskurin on 27.09.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_14_HW_MappingWeather/Lesson_14_HW_MappingWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_14_HW_MappingWeather
+//
+//  Created by Andrey Proskurin on 27.09.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
